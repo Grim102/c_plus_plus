9689d3c59cb60f9d860ee4b953b842e0da31c2aa
alex-endacott <endacott.alex@gmail.com>
2019-04-14 13:01:05 -0500

    add phase one of project

--- notepad-project/build-notepad-Desktop_Qt_5_11_2_GCC_64bit-Debug/moc_mainwindow.cpp
+++ notepad-project/build-notepad-Desktop_Qt_5_11_2_GCC_64bit-Debug/moc_mainwindow.cpp
@@ -6,7 +6,7 @@
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../notepad-project/notepad/mainwindow.h"
+#include "../notepad/mainwindow.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[12];
-    char stringdata0[255];
+    QByteArrayData data[4];
+    char stringdata0[59];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -34,25 +34,11 @@ static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
 QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
 QT_MOC_LITERAL(1, 11, 22), // "on_actionNew_triggered"
 QT_MOC_LITERAL(2, 34, 0), // ""
-QT_MOC_LITERAL(3, 35, 23), // "on_actionOpen_triggered"
-QT_MOC_LITERAL(4, 59, 26), // "on_actionSave_as_triggered"
-QT_MOC_LITERAL(5, 86, 24), // "on_actionPrint_triggered"
-QT_MOC_LITERAL(6, 111, 23), // "on_actionQuit_triggered"
-QT_MOC_LITERAL(7, 135, 23), // "on_actionCopy_triggered"
-QT_MOC_LITERAL(8, 159, 24), // "on_actionPaste_triggered"
-QT_MOC_LITERAL(9, 184, 22), // "on_actionCut_triggered"
-QT_MOC_LITERAL(10, 207, 23), // "on_actionUndo_triggered"
-QT_MOC_LITERAL(11, 231, 23) // "on_actionRedo_triggered"
+QT_MOC_LITERAL(3, 35, 23) // "on_actionOpen_triggered"
 
     },
     "MainWindow\0on_actionNew_triggered\0\0"
-    "on_actionOpen_triggered\0"
-    "on_actionSave_as_triggered\0"
-    "on_actionPrint_triggered\0"
-    "on_actionQuit_triggered\0on_actionCopy_triggered\0"
-    "on_actionPaste_triggered\0"
-    "on_actionCut_triggered\0on_actionUndo_triggered\0"
-    "on_actionRedo_triggered"
+    "on_actionOpen_triggered"
 };
 #undef QT_MOC_LITERAL
 
@@ -62,7 +48,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-      10,   14, // methods
+       2,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -70,26 +56,10 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   64,    2, 0x08 /* Private */,
-       3,    0,   65,    2, 0x08 /* Private */,
-       4,    0,   66,    2, 0x08 /* Private */,
-       5,    0,   67,    2, 0x08 /* Private */,
-       6,    0,   68,    2, 0x08 /* Private */,
-       7,    0,   69,    2, 0x08 /* Private */,
-       8,    0,   70,    2, 0x08 /* Private */,
-       9,    0,   71,    2, 0x08 /* Private */,
-      10,    0,   72,    2, 0x08 /* Private */,
-      11,    0,   73,    2, 0x08 /* Private */,
+       1,    0,   24,    2, 0x08 /* Private */,
+       3,    0,   25,    2, 0x08 /* Private */,
 
  // slots: parameters
-    QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void,
-    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
 
@@ -104,14 +74,6 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         switch (_id) {
         case 0: _t->on_actionNew_triggered(); break;
         case 1: _t->on_actionOpen_triggered(); break;
-        case 2: _t->on_actionSave_as_triggered(); break;
-        case 3: _t->on_actionPrint_triggered(); break;
-        case 4: _t->on_actionQuit_triggered(); break;
-        case 5: _t->on_actionCopy_triggered(); break;
-        case 6: _t->on_actionPaste_triggered(); break;
-        case 7: _t->on_actionCut_triggered(); break;
-        case 8: _t->on_actionUndo_triggered(); break;
-        case 9: _t->on_actionRedo_triggered(); break;
         default: ;
         }
     }
@@ -143,13 +105,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 10)
+        if (_id < 2)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 10;
+        _id -= 2;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 10)
+        if (_id < 2)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 10;
+        _id -= 2;
     }
     return _id;
 }
